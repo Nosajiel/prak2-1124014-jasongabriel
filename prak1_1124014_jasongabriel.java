ec9671fa25247289b23ed7725d583178b9ccb248
Nosaj <urghlol9@gmail.com>
2026-08-20 14:31:52 +0700

    branch

--- prak1_1124014_jasongabriel.java
+++ prak1_1124014_jasongabriel.java
@@ -3,8 +3,26 @@ public class prak1_1124014_jasongabriel {
         string name;
         double price;
         string category;
-        int totalMenu;
+        static int totalMenu;
         
+        Menu (string name, double price, string category){
+            this.name = name;
+            this.price = price;
+            this.category = category;
+            totalMenu++;
+        }
+        void showMenu(){
+            System.out.println("Name :" + name + "Price :" + price + "Category :" + category);
+        }
+        double discount(double percentage){
+            return price * (1 - percentage /100);
+        }
+        double changePrice(double newPrice){
+            this.price = newPrice;
+        }
+    }
+
+    static class Order{
         
     }
 }
