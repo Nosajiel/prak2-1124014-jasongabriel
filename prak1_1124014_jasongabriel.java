f7c4061a38bda0a958dd239a39d6bee684cb1099
Nosaj <urghlol9@gmail.com>
2026-08-20 13:43:16 +0700

    initial commit

---
+++ prak1_1124014_jasongabriel.java
@@ -0,0 +1,10 @@
+public class prak1_1124014_jasongabriel {
+    static class Menu{
+        string name;
+        double price;
+        string category;
+        int totalMenu;
+        
+        
+    }
+}
