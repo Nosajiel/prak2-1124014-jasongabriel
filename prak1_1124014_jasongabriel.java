96b65f2203f96a92ad9a259a7be1902cdcbd40a7
Nosaj <urghlol9@gmail.com>
2026-08-26 16:33:24 +0700

    update prak1

--- prak1_1124014_jasongabriel.java
+++ prak1_1124014_jasongabriel.java
@@ -23,6 +23,46 @@ public class prak1_1124014_jasongabriel {
     }
 
     static class Order{
-        
+        string orderId;
+        string customerName;
+        Menu menu;
+        int quantity;
+
+
+        Order(string orderId, string customerName, Menu menu, int quantity){
+            this.orderId = orderId;
+            this.customerName = customerName;
+            this.menu = menu;
+            this.quantity = quantity;
+        }
+
+        int calculateTotal(){
+            return menu.price * quantity;
+        }
+
+        void showOrder() {
+            System.out.println("Order ID     : " + orderId);
+            System.out.println("Customer     : " + customerName);
+            System.out.println("Menu         : " + menu.name);
+            System.out.println("Quantity     : " + quantity);
+            System.out.println("Total Payment: Rp" + calculateTotal());
+        }
+    }
+
+    public static void main (String[] args){
+        Menu.espresso = new Menu("Espresso", 20000, "Beverage");
+        Menu.croissant = new Menu("Croissant", 25000, "Food");
+        Menu.matcha = new Menu("Matcha", 22000, "Beverage");
+        Menu.sandwich = new Menu("Sandwich", 30000, "Food");
+
+        Menu[] menuList = {espresso, croissant, matcha, sandwich};
+        System.out.println("=== CAFE MENU ===");
+        for (Menu m : menuList){
+            m.showMenu();
+        }
+        System.out.println();
+
+        System.out.println("=== AFTER UPDATE ===");
+        System.out.println();
     }
 }
