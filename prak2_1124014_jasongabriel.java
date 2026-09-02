9cf5e0a7f9c20dbcc771740186f2a3c2bf52a39b
Nosaj <urghlol9@gmail.com>
2026-09-02 21:48:34 +0700

    Update getter setter

--- prak2_1124014_jasongabriel.java
+++ prak2_1124014_jasongabriel.java
@@ -11,20 +11,37 @@ abstract class Hewan {
     }
 
     // Getter dan Setter
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public int getAge() { return age; }
-    public void setAge(int age) { this.age = age; }
-    public String getFood() { return food; }
-    public void setFood(String food) { this.food = food; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
 
     public abstract String printSound();
 
     public String printData() {
         return "Jenis Hewan : " + getClass().getSimpleName() +
-               "\nNama : " + name +
-               "\nUmur : " + age + " tahun" +
-               "\nMakanan : " + food;
+                "\nNama : " + name +
+                "\nUmur : " + age + " tahun" +
+                "\nMakanan : " + food;
     }
 }
 
@@ -82,24 +99,33 @@ class Anjing extends HewanDarat {
     public Anjing(String name, int age, String food, int numberOfLegs) {
         super(name, age, food, numberOfLegs);
     }
+
     @Override
-    public String printSound() { return "Guk Guk"; }
+    public String printSound() {
+        return "Guk Guk";
+    }
 }
 
 class Kucing extends HewanDarat {
     public Kucing(String name, int age, String food, int numberOfLegs) {
         super(name, age, food, numberOfLegs);
     }
+
     @Override
-    public String printSound() { return "Meow Meow"; }
+    public String printSound() {
+        return "Meow Meow";
+    }
 }
 
 class Tikus extends HewanDarat {
     public Tikus(String name, int age, String food, int numberOfLegs) {
         super(name, age, food, numberOfLegs);
     }
+
     @Override
-    public String printSound() { return "Cit Cit"; }
+    public String printSound() {
+        return "Cit Cit";
+    }
 }
 
 // ========== Concrete Class Laut ==========
@@ -107,24 +133,33 @@ class Ikan extends HewanLaut {
     public Ikan(String name, int age, String food, double depth) {
         super(name, age, food, depth);
     }
+
     @Override
-    public String printSound() { return "Blub Blub"; }
+    public String printSound() {
+        return "Blub Blub";
+    }
 }
 
 class Hiu extends HewanLaut {
     public Hiu(String name, int age, String food, double depth) {
         super(name, age, food, depth);
     }
+
     @Override
-    public String printSound() { return "Splash / Silent Predator"; }
+    public String printSound() {
+        return "Splash / Silent Predator";
+    }
 }
 
 class Paus extends HewanLaut {
     public Paus(String name, int age, String food, double depth) {
         super(name, age, food, depth);
     }
+
     @Override
-    public String printSound() { return "Echolocation Click / Whistle"; }
+    public String printSound() {
+        return "Echolocation Click / Whistle";
+    }
 }
 
 // ========== Concrete Class Udara ==========
@@ -132,38 +167,47 @@ class Burung extends HewanUdara {
     public Burung(String name, int age, String food, double wingSpan) {
         super(name, age, food, wingSpan);
     }
+
     @Override
-    public String printSound() { return "Cuit Cuit"; }
+    public String printSound() {
+        return "Cuit Cuit";
+    }
 }
 
 class Elang extends HewanUdara {
     public Elang(String name, int age, String food, double wingSpan) {
         super(name, age, food, wingSpan);
     }
+
     @Override
-    public String printSound() { return "Kreeee / Screech"; }
+    public String printSound() {
+        return "Kreeee / Screech";
+    }
 }
 
 class Bebek extends HewanUdara {
     public Bebek(String name, int age, String food, double wingSpan) {
         super(name, age, food, wingSpan);
     }
+
     @Override
-    public String printSound() { return "Kwek Kwek"; }
+    public String printSound() {
+        return "Kwek Kwek";
+    }
 }
 
 public class prak2_1124014_jasongabriel {
     public static void main(String[] args) {
         Hewan[] daftarHewan = new Hewan[] {
-            new Anjing("Doggy", 3, "Daging / Tulang", 4),
-            new Kucing("Milo", 2, "Ikan", 4),
-            new Tikus("Jerry", 1, "Keju", 4),
-            new Ikan("Nemo", 1, "Plankton", 15.0),
-            new Hiu("Megalo", 10, "Daging", 500.0),
-            new Paus("Bluey", 20, "Krill", 1000.0),
-            new Burung("Pipit", 1, "Biji-bijian", 0.2),
-            new Elang("Garuda", 5, "Daging", 2.1),
-            new Bebek("Donald", 2, "Dedak", 0.8)
+                new Anjing("Doggy", 3, "Daging / Tulang", 4),
+                new Kucing("Milo", 2, "Ikan", 4),
+                new Tikus("Jerry", 1, "Keju", 4),
+                new Ikan("Nemo", 1, "Plankton", 15.0),
+                new Hiu("Megalo", 10, "Daging", 500.0),
+                new Paus("Bluey", 20, "Krill", 1000.0),
+                new Burung("Pipit", 1, "Biji-bijian", 0.2),
+                new Elang("Garuda", 5, "Daging", 2.1),
+                new Bebek("Donald", 2, "Dedak", 0.8)
         };
 
         for (int i = 0; i < daftarHewan.length; i++) {
